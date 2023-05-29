0f6d88d25e76447e09e4754463f6df1c88369f5a
Samuele Facenda <samuele.facenda@gmail.com>
2023-05-29 08:00:08 +0200

    operator addes

--- src/main/kotlin/vectors/Vector.kt
+++ src/main/kotlin/vectors/Vector.kt
@@ -5,4 +5,20 @@ interface Vector {
     fun subtract(v: Vector): Vector
     fun multiply(s: Double): Vector
     fun divide(s: Double): Vector
+
+    operator fun plus(v: Vector): Vector {
+        return add(v)
+    }
+
+    operator fun minus(v: Vector): Vector {
+        return subtract(v)
+    }
+
+    operator fun times(s: Double): Vector {
+        return multiply(s)
+    }
+
+    operator fun div(s: Double): Vector {
+        return divide(s)
+    }
 }
