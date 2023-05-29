d08dff5b2102a150f42a087ca37676135cc9d035
Samuele Facenda <samuele.facenda@gmail.com>
2023-05-29 12:28:38 +0200

    start ball container

--- src/main/kotlin/balls/BallContainer.kt
+++ src/main/kotlin/balls/BallContainer.kt
@@ -1,9 +1,26 @@
 package balls
 
+import vectors.PolarVector
+import kotlin.random.Random
+
 class BallContainer(
     private val height: Double = 0.0,
     private val width: Double = 0.0,
     private val numberOfChild: Int = 1,
+    private val numberOfLayer: Int = 1
 ) {
+    private val balls: List<Ball> = (1..numberOfChild).map { getNewBall(it) }
 
+    private fun getNewBall(index: Int): Ball{
+        return Ball(
+            numberOfChildren = numberOfChild,
+            layer = numberOfLayer,
+            startX = width / 2,
+            startY = height / 2,
+            startSpeed = PolarVector(
+                Random.nextDouble(0.0, 10.0),
+                Random.nextDouble(0.0, 2 * Math.PI)
+            )
+        )
+    }
 }
