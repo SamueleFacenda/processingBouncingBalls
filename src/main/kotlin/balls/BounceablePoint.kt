41d5cd4f29b0cbf5427f30038150a3bdcd24a398
Samuele Facenda <samuele.facenda@gmail.com>
2023-05-29 11:31:27 +0200

    bounce computation

--- src/main/kotlin/balls/BounceablePoint.kt
+++ src/main/kotlin/balls/BounceablePoint.kt
@@ -1,6 +1,7 @@
 package balls
 
 import vectors.CartesianVector
+import vectors.PolarVector
 import vectors.Vector
 
 class BounceablePoint(
@@ -17,6 +18,59 @@ class BounceablePoint(
     }
 
     fun bounceOn(other: BounceablePoint): BounceablePoint {
-        TODO("studiare un po di fisica e implementare la funzione")
+
+        // m1 * v1 * cos B1 + m2 * v2 * cos B2 = m1 * u1 * cos A1 + m2 * u2 * cos A2
+        // v1 * sin B1 = u1 * sin A1
+        // v2 * sin B2 = u2 * sin A2
+        // v2 * cos B2 - v1 * cos B1 = u1 * cos A1 - u2 * cos A2
+
+        val angleOfCentersLine = Math.atan2(other.y - y, other.x - x)
+        val Alpha1 = angleOfCentersLine - Math.atan2(speed.getY(), speed.getX())
+        val Alpha2 = angleOfCentersLine - Math.atan2(other.speed.getY(), other.speed.getX())
+        val u1 = speed.getLength()
+        val u2 = other.speed.getLength()
+
+        // v1 = (u1 * sin A1) / sin B1
+        // v2 = (u2 * sin A2) / sin B2
+
+        // u2 * sin A2 / sin B2 * cos B2 - u1 * sin A1 / sin B1 * cos B1 = u1 * cos A1 - u2 * cos A2
+        // u2 * sin A2 * cos B2 / sin B2 - u1 * sin A1 * cos B1 / sin B1 = val1
+        val val1 = u1 * Math.cos(Alpha1) - u2 * Math.cos(Alpha2)
+
+        // u2 * sin A2 * cos B2 / sin B2 =  u1 * sin A1 * cos B1 / sin B1  + val1
+        // cos B2 / sin B2 = (u1 * sin A1 * cos B1 / sin B1  + val1) / (u2 * sin A2)
+        // cos B2 = (u1 * sin A1 * cos B1 / sin B1 / u2 * sin A2 + val1 / u2 / sin A2) * sin B2
+        // cos B2 / sin B2 = val2 * cos B1 / sin B1 + val3
+        val val2 = u1 * Math.sin(Alpha1) / u2 / Math.sin(Alpha2)
+        val val3 = val1 / u2 / Math.sin(Alpha2)
+
+        // (sin B2 / cos B2) ^ -1 = val2 * (sin B1 / cos B1) ^ -1 + val3
+        // 1 / tan B2 = val2 /tan B1 + val3
+        // tan B2 = 1 / (val2 /tan B1 + val3)
+        // B2 = atan(1 / (val2 /tan B1 + val3))
+
+        // m1 * v1 * cos B1 + m2 * v2 * cos B2 = m1 * u1 * cos A1 + m2 * u2 * cos A2
+        // m1 * (u1 * sin A1) / sin B1 * cos B1 + m2 * (u2 * sin A2) / sin B2 * cos B2 = m1 * u1 * cos A1 + m2 * u2 * cos A2
+        // m1 * (u1 * sin A1) / sin B1 * cos B1 + m2 * (u2 * sin A2) * (val2 * cos B1 / sin B1 + val3) = m1 * u1 * cos A1 + m2 * u2 * cos A2
+        // (cos B1 / sin B1) ( m1 * u1 * sin A1 + m2 * u2 * sin A2 * val2) + m2 * u2 * sin A2 * val3 = m1 * u1 * cos A1 + m2 * u2 * cos A2
+        // (cos B1 / sin B1) ( m1 * u1 * sin A1 + m2 * u2 * sin A2 * val2) = m1 * u1 * cos A1 + m2 * u2 * cos A2 - m2 * u2 * sin A2 * val3
+        // (cos B1 / sin B1) = (m1 * u1 * cos A1 + m2 * u2 * cos A2 - m2 * u2 * sin A2 * val3) / ( m1 * u1 * sin A1 + m2 * u2 * sin A2 * val2)
+        // sin B1 / cos B1 = ( m1 * u1 * sin A1 + m2 * u2 * sin A2 * val2) / (m1 * u1 * cos A1 + m2 * u2 * cos A2 - m2 * u2 * sin A2 * val3)
+        // B1 = atan(( m1 * u1 * sin A1 + m2 * u2 * sin A2 * val2) / (m1 * u1 * cos A1 + m2 * u2 * cos A2 - m2 * u2 * sin A2 * val3))
+
+        val B1 = Math.atan2(
+            mass * u1 * Math.sin(Alpha1) + other.mass * u2 * Math.sin(Alpha2) * val2,
+            mass * u1 * Math.cos(Alpha1) + other.mass * u2 * Math.cos(Alpha2) - other.mass * u2 * Math.sin(Alpha2) * val3
+        )
+        /*val B2 = Math.atan2(
+            1.0,
+            val2 / Math.tan(B1) + val3
+        )*/
+        val v1 = (u1 * Math.sin(Alpha1)) / Math.sin(B1)
+        //val v2 = (u2 * Math.sin(Alpha2)) / Math.sin(B2)
+
+        val newSpeed = PolarVector(v1, angleOfCentersLine + B1)
+
+        return BounceablePoint(mass, newSpeed, x, y)
     }
 }
